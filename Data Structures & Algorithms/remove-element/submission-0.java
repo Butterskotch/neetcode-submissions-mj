8ea989e88734c003d1b8da01a60da7db57c4d7d2
Butterskotch <mayank8486@yahoo.com>
2026-04-04 23:54:13 +0530

    Add: remove-element - submission-0

---
+++ Data Structures & Algorithms/remove-element/submission-0.java
@@ -0,0 +1,19 @@
+class Solution 
+{
+    public int removeElement(int[] nums, int val) 
+    {
+        List<Integer> out=new ArrayList<>();
+        for(int i=0;i<nums.length;i++)
+        {
+            if(nums[i]!=val)
+            {
+                out.add(nums[i]);
+            }
+        }
+        for(int i=0;i<out.size();i++)
+        {
+            nums[i]=out.get(i);
+        }
+        return out.size();
+    }
+}
