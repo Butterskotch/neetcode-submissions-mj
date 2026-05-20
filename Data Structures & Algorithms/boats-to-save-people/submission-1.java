fe6d10c2c6e14ce9b303e460b2e623b27a2a8abd
Mayank Joshi <mayank8486@yahoo.com>
2026-05-20 11:27:16 +0530

    Add: boats-to-save-people - submission-1

---
+++ Data Structures & Algorithms/boats-to-save-people/submission-1.java
@@ -0,0 +1,33 @@
+class Solution 
+{
+    public int numRescueBoats(int[] people, int limit) 
+    {
+        Arrays.sort(people);
+        int f=0,r=people.length-1;
+        int output=0;
+        while(f<r)
+        {
+            if(people[r]==limit)
+            {
+                output++;
+                r--;
+            }
+            else if(people[r]+people[f]<=limit)
+            {
+                output++;
+                r--;
+                f++;
+            }
+            else
+            {
+                output++;
+                r--;
+            }
+        }
+        if(f==r)
+        {
+            output++;
+        }
+        return output;
+    }
+}
