db082b0542de672f3141822bda0ea3e3730ee596
Mayank Joshi <mayank8486@yahoo.com>
2026-06-24 20:30:30 +0530

    Add: path-crossing - submission-3

---
+++ Data Structures & Algorithms/path-crossing/submission-3.java
@@ -0,0 +1,35 @@
+class Solution 
+{
+    public boolean isPathCrossing(String path) 
+    {
+        int x=0,y=0;
+        Set<String> visited = new HashSet<>();
+        visited.add(x +","+ y);
+        for(char c : path.toCharArray())
+        {
+            if(c=='N')
+            {
+                y++;
+            }
+            else if(c=='S')
+            {
+                y--;
+            }
+            else if(c=='E')
+            {
+                x++;
+            }
+            else
+            {
+                x--;
+            }
+            String position= x +","+ y;
+            if(visited.contains(position))
+            {
+                return true;
+            }
+            visited.add(position);
+        }
+        return false;
+    }
+}
